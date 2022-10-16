107d942290ec9f4fafa8a1b44e1272a5ef7918d7
Replit user <>
2022-10-16 05:26:11 +0000

    dll fixes

---
+++ main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include "DLL.c"
+int main()
+{
+  dll dl;
+  createdll(&dl);
+  insertIntoEmpty(&dl,100);
+  insertBegin(&dl,50);
+  insertEnd(&dl,200);
+  insertEnd(&dl,100000);
+  insertAtPosition(&dl,400,2);
+  
+  printf("\n%d deleted\n",deleteBegin(&dl));
+  deleteEnd(&dl);
+  deleteAtPosition(&dl,2);
+  
+  
+  display(&dl);
+  printf("\nLength of the doubly linked list is :  %d",length(&dl));
+  return 0;
+}
